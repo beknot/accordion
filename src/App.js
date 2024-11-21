76e182f6531c28d81e238ddabb7efd988e2efa26
binod.chakradhar <binod.chakradhar@ebpearls.com>
2024-11-21 21:44:54 +0545

    Accordion added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Accordian from './components/accordion';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Accordian componenrt */}
+      <Accordian/>
+    </div>
+  );
+}
+
+export default App;
